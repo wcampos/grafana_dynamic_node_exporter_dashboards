9a3a9a502887b2d3fc1cf6a1b1f53a2224639e01
WilCampos <wilbert.campos@gmail.com>
2020-01-24 18:02:32 -0800

    Create generate_dasboards.py

---
+++ generate_dasboards.py
@@ -0,0 +1,57 @@
+import boto3,json,tabulate,pprint,itertools,os,random,string,pwd,grp
+from shutil import copyfile
+
+Instances=[]
+TemplateFile="./templates/node_exporter_dashboard.json"
+GrafanaDashPath="/var/lib/grafana/dashboards"
+GrafanaUID=pwd.getpwnam("grafana").pw_uid
+GrafanaGID=grp.getgrnam("grafana").gr_gid
+HDPDashPath=GrafanaDashPath+"/hdp"
+
+def createDirNPermissions(dirin):
+    if not os.path.exists(dirin):
+       os.makedirs(dirin)
+       os.chown(dirin,GrafanaUID,GrafanaGID)
+
+def randomString(stringLength=10):
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(stringLength))
+
+def replaceInFile(fin,key,val):
+    with open(fin, 'r') as f:
+        newlines = []
+        for line in f.readlines():
+            newlines.append(line.replace(key,val))
+    with open(fin, 'w') as f:
+        for line in newlines:
+           f.write(line)
+
+createDirNPermissions(GrafanaDashPath)
+createDirNPermissions(HDPDashPath)
+
+ec2 = boto3.client('ec2')
+filters = [
+    {
+        'Name': 'tag:opsworks:stack',
+        'Values': ['edp-prod01-ops-stack']
+    }
+]
+list_instances=json.dumps(ec2.describe_instances(Filters=filters), indent=4, sort_keys=True, default=str)
+data = json.loads(list_instances)
+idict={}
+ilist=[]
+
+for reservations in data['Reservations']:
+    for instance in reservations['Instances']:
+        for tag in instance['Tags']:
+            if tag['Key'] == 'Name':
+               iName =  tag['Value']
+               iName = iName.replace(" ","")
+               iPrivateDNS = instance['PrivateDnsName']
+               dashUid = randomString(10)
+               path=HDPDashPath+"/"+iName+"-dashboards.json"
+               copyfile(TemplateFile,path)
+               os.chown(path,GrafanaUID,GrafanaGID)
+               Replacements={'MACHINENAME': iName, 'MACHINEIPADDR': iPrivateDNS, 'DASHBOARDUID': dashUid}
+               for k,v in Replacements.items():
+                   replaceInFile(path,k,v)
